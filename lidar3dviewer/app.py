25f2b054df4c89ad19177b9887733b939df37414
vdhorstruben <rubenvanderhorst@live.nl>
2022-12-13 13:27:42 +0100

    geometrie model

--- lidar3dviewer/app.py
+++ lidar3dviewer/app.py
@@ -1,11 +1,9 @@
 from viktor import ViktorController
-from viktor.parametrization import ViktorParametrization
-
-
-class Parametrization(ViktorParametrization):
-    pass
-
-
+from viktor.geometry import SquareBeam
+from viktor.views import GeometryView, GeometryResult
 class Controller(ViktorController):
-    label = 'My Entity Type'
-    parametrization = Parametrization
+    label = "My Beam"
+    @GeometryView("My 3D model", duration_guess=1)
+    def beam_visualisation(self, params, **kwargs):
+        beam = SquareBeam(length_x=0.3, length_y=0.5, length_z=3)
+        return GeometryResult(beam)
